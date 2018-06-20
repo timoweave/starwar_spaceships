d6c5e75afb364ab15e70f7092187228649bb758f
Timothy Shiu <timoweave@gmail.com>
2018-06-20 01:17:48 -0400

    add detail view

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
 Installation
 ############
 
+`git clone`
 `npm install`
 `npm start`
+
+Behavioral/Styling
+##################
+
+There is a header bar just read "Watto's Space Eporiume". When a given
+route is not matched/recognized, it will redirected to `/starships`.
+
+When the mouse is hover enter a starship in `/starships`, it will
+scale up to 105% in 0.5 sec.  When the mouse is hover exit a starship,
+it will shrink down to 95% in 0.5 sec.
+
+Routing
+#######
+1. starships   show all the available starship available from Watto Space Eporium
+1. starships/:id show detail of a particular starship
+1. images  (misc) show list of images for starships
+1. images/:id (misc) show one image
+1. about (misc) show my info
 
 Stack
 #####
 
 1. use [create-react-app](https://github.com/facebookincubator/create-react-app). to 
-   quickly setup babel, webpack, and hmr
+   quickly setup babel, webpack, and hmr (parcel-bundle could be used, but it is a 
+   bit cutting edge if there is time)
 1. use apollo-link-rest graphql to wrap rest api (easy to filter and specify data needed), 
    and using apollo-link-state, manage client side state management (in this case just 
-   match the image with the starship)
+   match the image with the starship) and apollo-cache-presistent to use localStorage.
+1. add service worker to speed up asset loading for faster response and faster hot module reload.
+   (especially useful for large number of image files)
 1. image reference are from https://www.starwars.com/databank/b-wing-fighter
 1. api https://swapi.co/api/ and https://demo7475333.mockable.io/spaceships
 
--- src/Starship.js
+++ src/Starship.js
@@ -1,10 +1,22 @@
 import React from "react";
 import {Query} from "react-apollo";
 import gql from "graphql-tag";
+import "./Starship.css";
 
 const Starship = props => {
-  const {match: {params: {model}}} = props;
-  const query = gql`
+  const {
+    match: {
+      params: {model},
+    },
+  } = props;
+  const query_image = gql`
+    query($model: String!) {
+      image(id: $model) @client {
+        url
+      }
+    }
+  `;
+  const query_ship = gql`
     query($model: String!) {
       starship(id: $model) @rest(type: "Starship", path: "starships/:id") {
         MGLT
@@ -25,45 +37,72 @@ const Starship = props => {
   `;
 
   return (
-    <Query query={query} variables={{model}}>
+    <Query query={query_image} variables={{model}}>
       {({loading, error, data}) => {
-        if (loading) {
-          return <h4>Loading...</h4>;
+        if (loading || error) {
+          return loading ? <h4>Loading...</h4> : <h4>{error.message}</h4>;
         }
-        if (error) {
-          return <h4>{error.message}</h4>;
-        }
-        const {starship} = data;
-
         const {
-          name,
-          model,
-          manufacturer,
-          cost_in_credits,
-          max_atmosphering_speed,
-          crew,
-          consumables,
-          hyperdrive_rating,
-          passengers,
-          starship_class,
-          length,
-        } = starship;
+          image: {url},
+        } = data;
+
         return (
-          <div>
-            <h1>
-              {name} ({props.match.params.model})
-            </h1>
-            <p>model: {model}</p>
-            <p>make: {manufacturer}</p>
-            <p>speed: {max_atmosphering_speed}</p>
-            <p>cost: {cost_in_credits}</p>
-            <p>length: {length}</p>
-            <p>class: {starship_class}</p>
-            <p>hyperdriver: {hyperdrive_rating}</p>
-            <p>consumable: {consumables}</p>
-            <p>passengers: {passengers}</p>
-            <p>crew: {crew}</p>
-          </div>
+          <Query query={query_ship} variables={{model}}>
+            {({loading, error, data}) => {
+              if (loading || error) {
+                return loading ? <h4>Loading...</h4> : <h4>{error.message}</h4>;
+              }
+              const {starship} = data;
+
+              const {
+                name,
+                model,
+                manufacturer,
+                cost_in_credits,
+                max_atmosphering_speed,
+                crew,
+                consumables,
+                hyperdrive_rating,
+                passengers,
+                starship_class,
+                length,
+              } = starship;
+              return (
+                <div className="StarshipContainer">
+                  <div className="StarshipLeft">
+                    <h1>
+                      {name} ({props.match.params.model})
+                    </h1>
+                    <img alt={model} src={url} width="100%" />
+                    <p>
+                      Consequat nisl, vel pretium lectus quam id leo in vitae
+                      turpis massa sed elementum tempus egestas sed sed risus
+                      pretium quam vulputate dignissim suspendisse! A diam
+                      maecenas sed enim ut sem viverra aliquet eget sit amet
+                      tellus cras adipiscing enim eu turpis egestas pretium
+                      aenean pharetra, magna ac placerat!
+                    </p>
+                  </div>
+                  <div className="StarshipRight">
+                    <h1>Tech Specs</h1>
+                    <p>model: {model}</p>
+                    <p>make: {manufacturer}</p>
+                    <p>speed: {max_atmosphering_speed}</p>
+                    <p>cost: {cost_in_credits}</p>
+                    <p>length: {length}</p>
+                    <p>class: {starship_class}</p>
+                    <p>hyperdriver: {hyperdrive_rating}</p>
+                    <p>consumable: {consumables}</p>
+                    <p>passengers: {passengers}</p>
+                    <p>crew: {crew}</p>
+                  </div>
+                  <div className="StarshipBuy">
+                    <input name="" type="button" value="Buy" />
+                  </div>
+                </div>
+              );
+            }}
+          </Query>
         );
       }}
     </Query>
